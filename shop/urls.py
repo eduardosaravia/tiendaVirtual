59ead8a4d7a3f32b42a5637c5b95b62c0a316795
Eduardo Saravia <carlos.saravia@usil.pe>
2023-02-01 22:40:57 -0500

    Create product

--- shop/urls.py
+++ shop/urls.py
@@ -6,4 +6,5 @@ app_name = "shop"
 urlpatterns = [
     path('',index, name="index"),
     path('product/<int:pk>/',product_detail,name='product_detail' ),
+    path('product/create/',produt_create,name='product_create'),
 ]
--- shop/views.py
+++ shop/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 # GET OBJECT ERROR 404
-from django.shortcuts import get_object_or_404
+from django.shortcuts import get_object_or_404, redirect
 from .models import *
 
 def index(request):
@@ -21,3 +21,24 @@ def product_detail(request, pk):
         "product" : product
     }
     return render(request,'shop/product_detail.html',context)
+
+def produt_create(request):
+    if request.method == "POST":
+        name = request.POST.get('name')
+        descripcion = request.POST.get('descripcion')
+        price = request.POST.get('price')
+        category = request.POST.get('category')
+        category = Category.objects.get(id=category)
+        product = Product.objects.create(
+            name = name,
+            descripcion = descripcion,
+            price = price,
+            category = category
+        )
+        return redirect('shop:product_detail',pk = product.id)
+
+    context ={
+        'categories' : Category.objects.all()
+
+    }
+    return render(request,'shop/product_create.html',context)
