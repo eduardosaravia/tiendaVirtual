795e325ac0d7a33eb4d61b8c244e915b6b9e054b
Eduardo Saravia <carlos.saravia@usil.pe>
2023-10-01 13:23:29 -0500

    Django admin - models - requirements

--- shop/admin.py
+++ shop/admin.py
@@ -12,3 +12,12 @@ class ProductAdmin(admin.ModelAdmin):
     list_filter = ('category','is_active')
     search_fields = ('name','descripcion')
     
+
+class CartItemInline(admin.TabularInline):
+    model = CartItem
+    extra = 0
+
+@admin.register(Cart)
+class CartAdmin(admin.ModelAdmin):
+    list_display = ('id','total_items','total_price')
+    inlines = [CartItemInline]
--- shop/models.py
+++ shop/models.py
@@ -24,3 +24,36 @@ class Product(models.Model):
     @property
     def category_name(self):
         return self.category.name
+
+class Cart(models.Model):
+    pass
+    class Meta:
+        verbose_name = "Shopping Cart"
+        verbose_name_plural = "Shopping Carts"
+    
+    def __str__(self) -> str:
+        return f"Shopping Cart {self.id}"
+
+    @property
+    def total_items(self):
+        return self.items.count()
+    
+    @property
+    def total_price(self):
+        return sum([item.total_price for item in self.items.all()])
+
+class CartItem(models.Model):
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE, related_name="items")
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.IntegerField()
+    
+    class Meta:
+        verbose_name = "Shopping Cart Item"
+        verbose_name_plural = "Shopping Cart Items"
+
+    def __str__(self) -> str:
+        return self.product.name
+    
+    @property
+    def total_price(self):
+        return self.product.price * self.quantity
